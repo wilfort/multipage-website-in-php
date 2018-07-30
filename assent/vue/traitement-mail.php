27b16ab740fe58f7599cd70787d4e7e132abe558
wilfort <wilfort.stephane@hotmail.fr>
2018-07-30 16:43:03 +0200

    project php

--- assent/vue/traitement-mail.php
+++ assent/vue/traitement-mail.php
@@ -1,6 +1,58 @@
 <?php 
+    /**
+     * This example shows settings to use when sending via Google's Gmail servers.
+     * This uses traditional id & password authentication - look at the gmail_xoauth.phps
+     * example to see how to use XOAUTH2.
+     * The IMAP section shows how to save this message to the 'Sent Mail' folder using IMAP commands.
+     */
+    //Import PHPMailer classes into the global namespace
+    use PHPMailer\PHPMailer\PHPMailer;
+    require './vendor/autoload.php';
     
+    $nom="";$prenom="";$email="";$prenom="";
+    if(isset($_POST['envoie'])){
     
+        // on initie un tableau qui va contenir toute erreur potentielle.
+        $errors = array();
+        // 1. Sanitisation
+        $genre = $_POST['genre'];
+        echo $genre."<br>";
+        $nom = filter_var($_POST['nom'],FILTER_SANITIZE_STRING);
+        echo $nom."<br>";
+        $prenom = filter_var($_POST['prenom'],FILTER_SANITIZE_STRING);
+        echo $prenom."<br>";
+        $email = filter_var($_POST['adresse'], FILTER_SANITIZE_EMAIL);
+        echo $email."<br>";
+        $objet = $_POST['objet'];
+        echo $objet."<br>";
+        $message = filter_var($_POST['message'],FILTER_SANITIZE_STRING);
+        echo $message."<br>";
+        $type = $_POST['type'];
+        echo $type;
+        // 2. Validation
+        if (false === filter_var($nom, FILTER_VALIDATE_STRING)) {
+            $errors['nom'] =  "Cette adresse est invalide.";
+            }
+
+        if (false === filter_var($prenom, FILTER_VALIDATE_STRING)) {
+            $errors['prenom'] =  "Cette adresse est invalide.";
+        }
+
+        if (false === filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors['email'] =  "Cette adresse est invalide.";
+        }
+
+        if (false === filter_var($message, FILTER_VALIDATE_STRING)) {
+        $errors['message'] =  "Cette adresse est invalide.";
+        }
+
+        // 3. Exécution
+        if (count($errors)> 0){
+            echo 'Il y a des erreurs!<br>';
+            echo $email;
+            print_r($errors);
+            exit;
+        }
+        //include("mail.php");
     
-    
-    include("mail.php");
+    }
--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 
-    //include("/assent/vue/traitement-mail.php");
+    include("./assent/vue/traitement-mail.php");
 
 ?>
 <!DOCTYPE html>
@@ -22,69 +22,78 @@
     ?>
     <section>
         
-                <form action="#" method="post">
+                <form action="#" method="post" enctype="multipart/form-data">
                     <div class="row">
                         <div class="col-12">
                             <h4>Contact</h4>
                         </div>
+
                         <div class="col-4">
-                            <label for="gender">titre</label>
+                            <label for="genre">titre</label>
                         </div>
                         <div class="col-8">
-                            <input type="radio" name="gender" id="" value="Mme"> Mme
-                            <input type="radio" name="gender" id="" value="Melle"> Melle
-                            <input type="radio" name="gender" id="" value="Mr."> Mr. 
+                            <input type="radio" name="genre" id="" value="Mme" checked> Mme
+                            <input type="radio" name="genre" id="" value="Melle"> Melle
+                            <input type="radio" name="genre" id="" value="Mr."> Mr. 
                         </div>
+
                         <div class="col-4">
                             <label for="nom">nom</label>
                         </div>
                         <div class="col-8">
                             <input type="text" name="nom" id="nom">
                         </div>
+
                         <div class="col-4">
                         <label for="prenom">prénon</label>
                         </div>
                         <div class="col-8">
                         <input type="text" name="prenom" id="prenom">
                         </div>
+
                         <div class="col-4">
                         <label for="adresse">email</label>
                         </div>
                         <div class="col-8">
                         <input type="email" name="adresse" id="adresse">
                         </div>
+
                         <div class="col-4">
-                        <label for="objet">titre</label>
+                        <label for="objet">objet</label>
                         </div>
                         <div class="col-8">
                             <select name="objet" id="">
-                                <option value="info">demande d'informations</option>
+                                <option value="info" selected>demande d'informations</option>
                                 <!-- <option value=""></option>
                                 <option value=""></option>
                                 <option value=""></option> -->
                             </select>
                         </div>
+
                         <div class="col-4">
                             <label for="message">Votre message</label>
                         </div>
                         <div class="col-8">
                             <textarea name="message" id="" cols="30" rows="10"></textarea>
                         </div>
-                        <div class="col-4">
+
+                        <!-- <div class="col-4">
                             <label for="file">documents</label>
                         </div>
                         <div class="col-8">
                             <input type="file" name="" id=""><br>
-                        </div>
+                        </div> -->
+
                         <div class="col-4">
                             <label for="type">titre</label>
                         </div>
                         <div class="col-8">
-                            <input type="radio" name="type" id="" value="HTML"> HTML
+                            <input type="radio" name="type" id="" value="HTML" checked> HTML
                             <input type="radio" name="type" id="" value="Texte"> Texte
                         </div>
+
                         <div class="col-12">
-                            <button type="submit">Contactez-moi</button>
+                            <button type="submit" name="envoie">Contactez-moi</button>
                         </div>
                     </div>
                 </form>
@@ -93,7 +102,7 @@
     <?php
 
         include("./assent/vue/footer.php");
-
+        print_r($_POST);
     ?>		
 </body>
 </html>
