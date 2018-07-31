c1401436a4c4bbdc001d62ae5f1ed260d24a8066
quang-le <quangle.bxl@gmail.com>
2018-07-31 12:11:54 +0200

    log des upload de fichier dans log.txt OK, filtre par extension OK

--- assets/php/upload.php
+++ assets/php/upload.php
@@ -3,13 +3,14 @@
 require ('./vendor/verot/class.upload.php/src/class.upload.php');
 
 function uploader (){
+    $today= date('d/m/Y H:i:s');
     $handle = new upload($_FILES['upload']);
     if ($handle->uploaded){
         if ($handle->file_src_name_ext!='bmp' && $handle->file_is_image=='true'){
             $handle->process('./assets/uploads/');
             if ($handle->processed){
                 echo 'image uploaded';
-                file_put_contents('./log.txt','uploaded image'.$handle->file_src_name.'to '.$handle->file_dst_name,FILE_APPEND);
+                file_put_contents('./assets/log.txt',$today.' upload de '.$handle->file_src_name.' vers '.$handle->file_dst_name."\n",FILE_APPEND);
             }
             else{
                 echo 'error'.$handle->error;
@@ -17,11 +18,12 @@ function uploader (){
         }
         else{
             echo 'type de fichier non supporté';
+            file_put_contents('./assets/log.txt',$today.' upload de '.$handle->file_src_name.':fichier non-supporté '."\n",FILE_APPEND);
         }
     }
 }
 uploader();
 
-
+//phpinfo();
 
 ?>
