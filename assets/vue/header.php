cea2602d3ff34524e47e75e064b8b95c3c73e4af
wilfort <wilfort.stephane@hotmail.fr>
2018-07-31 16:52:58 +0200

    project php letilt

--- assets/vue/header.php
+++ assets/vue/header.php
@@ -1,4 +1,35 @@
 <header>
-
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+        <div class="navbar-brand" id="band">
+            <img src="./assets/img/logo.png" alt="logo de letilt">
+        </div>
+        
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="nav nav-pills nav-fill">
+                    <li class="nav-item">
+                        <a class="nav-link" href="index.php">
+                             Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="enfant.php">
+                             Enfant</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="prof.php">
+                             professionnel</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">
+                            Contact</a>
+                    </li>
+                    
+                </ul>
+            </div>
+        
+    </nav>
 
 </header>
