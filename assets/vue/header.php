29108beaf139765025974b80954ed4693bcdeb4d
wilfort <wilfort.stephane@hotmail.fr>
2018-08-01 11:39:39 +0200

    project php letilt

--- assets/vue/header.php
+++ assets/vue/header.php
@@ -1,5 +1,5 @@
 <header>
-    <nav class="navbar navbar-expand-md bg-light navbar-dark">
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
     <div class="row"></div>
         <span class="navbar-brand" id="band">
             <img src="./assets/img/logo.png" alt="logo de letilt">
@@ -11,20 +11,20 @@
             <div class="collapse navbar-collapse" id="collapsibleNavbar">
                 <ul class="nav nav-pills nav-fill">
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php">
+                        <a class="nav-link red" href="index.php">
                              Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="enfant.php">
+                        <a class="nav-link red" href="enfant.php">
                              Enfant</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="prof.php">
+                        <a class="nav-link red" href="prof.php">
                              professionnel</a>
                     </li>
                     
                     <li class="nav-item">
-                        <a class="nav-link" href="contact.php">
+                        <a class="nav-link red" href="contact.php">
                             Contact</a>
                     </li>
                     
--- contact.php
+++ contact.php
@@ -22,12 +22,22 @@
     
     ?>
     <section>
-        
+        <div class="row">
+            <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3">
+                <span class="linebar"></span>
+            </div>
+            <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 my-auto">
+                <h1 class="text-center red">Contact</h1>
+            </div>
+            <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3">
+            <span class="linebar"></span>
+            </div>
+        </div>
+        <div class="row">           
+            <div class="col-12">
                 <form action="#" method="post" enctype="multipart/form-data">
                     <div class="row">
-                        <div class="col-12">
-                            <h4>Contact</h4>
-                        </div>
+                        
 
                         <div class="col-md-12 col-lg-4">
                             <label for="genre">titre</label>
@@ -108,55 +118,54 @@
                         </div>
                     </div>
                 </form>
-                <div class="row">
-                    <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3 line">
-
-                    </div>
-                    <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6">
-                        <h1 class="main_header customColor">Nous avons besoin de dons pour vivre.</h1>
-                    </div>
-                    <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3 line">
-                        
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-xs-12">
-                        <h1>Don financier</h1>
-                    </div>
-                    <div class="col-xs-12">
-                        <img src="./assets/img/presentation.jpg" alt="">
-                    </div>
-                    <div class="col-xs-12">
-                        Vous avez envie de faire un don à une association ? Un projet qui a du sens, qui poursuit une mission auprès des jeunes, qui les pousse à réfléchir et qui favorise leur ouverture d’esprit ? Les dons faits à la Fondation donnent droit à une attestation fiscale à partir de € 40 (art. 145/33 CIR).
-                    </div>
-                    <div class="col-xs-12">
-                        Deux options s’offrent à vous:
-                    </div>
-                    <div class="col-xs-12">
-                        <ul>
-                            <li>
-                                Vos dons peuvent  être versés sur notre projet « <a href="http://www.bonnescauses.be/organisation/?id=0884345931&SearchTerm=odyssea%20asbl&autoSuggestWasUsed=5955,fr&Citydistance=5">Odyssea asbl</a> » (Site de la Fondation Roi Baudoin) jusqu’au 30/03/18. C’est la Fondation Roi Baudouin qui vous fournira l’attestation fiscale dont vous aurez besoin pour votre déductibilité.
-                            </li>
-                            <li>
-                                Les dons pour le projet peuvent être versés jusqu’au 30/03/18 au compte IBAN BE10 0000 0000 0404 – BIC: BPOTBEB1 de la Fondation Roi Baudouin avec la mention obligatoire ‘128/2887/00017’.
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-12">
-                        <h1>Don matériel</h1>
-                    </div>
-                    <div class="col-xs-12">
-                        Vous videz votre maison, vous terminez une activité, vous avez des objets de décoration à donner, du papier de dessin, du matériel d’animation à donner, des atlas, des cartes routières, des bijoux de fantaisie ? Vous ne voulez pas jeter ? Nous recyclons, par nos ateliers, dans nos décors, nous aimons transformer vos vieux objets et leur donner une seconde vie.
-                    </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3">
+                <span class="linebar"></span>
+            </div>
+            <div class="col-xs-12 col-sm-10 col-md-8 col-lg-6 my-auto">
+                <h1 class="text-center red">Nous avons besoin de dons pour vivre.</h1>
+            </div>
+            <div class="col-xs-0 col-sm-1 col-md-2 col-lg-3">
+                <span class="linebar"></span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center red">Don financier</h1>
+            </div>
+            <div class="col-12">
+                <img src="./assets/img/presentation.jpg" alt="">
+            </div>
+            <div class="col-12">
+                Vous avez envie de faire un don à une association ? Un projet qui a du sens, qui poursuit une mission auprès des jeunes, qui les pousse à réfléchir et qui favorise leur ouverture d’esprit ? Les dons faits à la Fondation donnent droit à <b>une attestation fiscale à partir de € 40</b> (art. 145/33 CIR).
+            </div>
+            <div class="col-12">
+                Deux options s’offrent à vous:
+            </div>
+            <div class="col-12">
+                <ul>
+                    <li>
+                        Vos dons peuvent  être versés sur notre projet « <a href="http://www.bonnescauses.be/organisation/?id=0884345931&SearchTerm=odyssea%20asbl&autoSuggestWasUsed=5955,fr&Citydistance=5" class="red">Odyssea asbl</a> » (Site de la Fondation Roi Baudoin) jusqu’au <b>30/03/18</b>. C’est la Fondation Roi Baudouin qui vous fournira l’attestation fiscale dont vous aurez besoin pour votre déductibilité.
+                    </li>
+                    <li>
+                        Les dons pour le projet peuvent être versés jusqu’au 30/03/18 au compte <b>IBAN BE10 0000 0000 0404 – BIC: BPOTBEB1</b> de la Fondation Roi Baudouin avec la mention obligatoire ‘128/2887/00017’.
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12">
+                <h1 class="text-center red">Don matériel</h1>
+            </div>
+            <div class="col-12">
+                Vous videz votre maison, vous terminez une activité, vous avez des objets de décoration à donner, du papier de dessin, du matériel d’animation à donner, des atlas, des cartes routières, des bijoux de fantaisie ? Vous ne voulez pas jeter ? Nous recyclons, par nos ateliers, dans nos décors, nous aimons transformer vos vieux objets et leur donner une seconde vie.
+            </div>
  
-                </div>
-                
+        </div>                
     </section>
-
 
     <?php
         include("./assets/vue/footer.php");
-        
     ?>		
 </body>
 </html>
