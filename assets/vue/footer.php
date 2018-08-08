5f1c3309220dc30c29b9b5c6050b8fb0b3e55fc4
wilfort <wilfort.stephane@hotmail.fr>
2018-08-08 11:38:44 +0200

    project php letilt

--- assets/vue/footer.php
+++ assets/vue/footer.php
@@ -1,109 +1,120 @@
 <footer class="footer">
     <div class="row">
-        <div class="col-md-12 col-lg-4">
+        <div class="col-md-12 col-lg-4 align-self-center">
             <h4>Le Tilt</h4>
             <p>Des activités, des stages, des ateliers pour les enfants</p>
         </div>
         <div class="col-md-12 col-lg-8">
             <div class="row">
-                <div class="col-2"><button name="prev" class="prev" onclick="botPart(-1)">
+                <div class="col-2 align-self-center text-center"><button name="prev" class="prev align-middle" onclick="botPart(-1)">
 								<b><<</b>
 							</button></div>
                 <div class="col-8">
                     <div class="myPart">
-                        <img src="./assets/img/tilt-logo-bw.jpg" alt="Le Brabant wallon">
+                        <img class="part mx-auto d-block" src="./assets/img/tilt-logo-bw.jpg" alt="Le Brabant wallon">
                     </div>
                     <div class="myPart">
-                        <img src="./assets/img/tilt-logo-spw.jpg" alt="Service public de Wallonie">
+                        <img class="part mx-auto d-block" src="./assets/img/tilt-logo-spw.jpg" alt="Service public de Wallonie">
                     </div>
                     <div class="myPart">
-                        <img src="./assets/img/Loterie-carr.jpg" alt="Loterie Nationale">
+                        <img class="part mx-auto d-block" src="./assets/img/Loterie-carr.jpg" alt="Loterie Nationale">
                     </div>
                     <div class="myPart">
-                        <img src="./assets/img/tilt-logo-fwb.jpg" alt="FEDERATION WALLONIE-BRUXELLES">
+                        <img class="part mx-auto d-block" src="./assets/img/tilt-logo-fwb.jpg" alt="FEDERATION WALLONIE-BRUXELLES">
                     </div>
                     <div class="myPart">
-                        <img src="./assets/img/tilt-logo-one.jpg" alt="OFFICE DE LA NAISSANCE ET DE L'ENFANCE">
+                        <img class="part mx-auto d-block" src="./assets/img/tilt-logo-one.jpg" alt="OFFICE DE LA NAISSANCE ET DE L'ENFANCE">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                        <img class="part mx-auto d-block" src="./assets/img/kiroundg.gif" alt="KIWANIS INTERNATIONAL">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                        <img class="part mx-auto d-block" src="./assets/img/tilt-logo-tvcom.jpg" alt="tV com BRABANT WALLON">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                        <img class="part mx-auto d-block" src="./assets/img/Région-Wallonne.jpg" alt="REGION WALLONNE">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                        <img class="part mx-auto d-block" src="./assets/img/tilt-logo-gazette-kid.jpg" alt="GAZETTE KIDS">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                        <img class="part mx-auto d-block" src="./assets/img/tilt-logo-article27.jpg" alt="article 27">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                        <img class="part mx-auto d-block" src="./assets/img/tilt-logo-cap48.jpg" alt="CAP 48">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                    <img class="part mx-auto d-block" src="./assets/img/110114logocommunerixensart.jpg" alt="BRABANT WALLON COMMUNE DE RIXENSART">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                    <img class="part mx-auto d-block" src="./assets/img/2013-GSK-Logo-with-Strapline.jpg" alt="GSK do more feel better live longer">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                    <img class="part mx-auto d-block" src="./assets/img/logo_reine.paola_.jpg" alt="Fondation Reine Paola">
                     </div>
                     <div class="myPart">
-                    <img src="./assets/img/" alt="">
+                    <img class="part mx-auto d-block" src="./assets/img/round_square_logo_fr_be.png" alt="Le Petit Moutard">
                     </div>
                 </div>
-                <div class="col-2"><button name="next" class="next" onclick="botPart(1)">
+                <div class="col-2 align-self-center text-center"><button name="next" class="next" onclick="botPart(1)">
                 <b>>></b>
 							</button></div>
                 <div class="col-12">
-                    <div class="viewPart" onclick="currentPart(1)">
-                        <img src="./assets/img/tilt-logo-bw.jpg" alt="Le Brabant wallon">
+                    <div class="row">
+                        <div class="viewPart col" onclick="currentPart(1)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/tilt-logo-bw.jpg" alt="Le Brabant wallon">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(2)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/tilt-logo-spw.jpg" alt="Service public de Wallonie">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(3)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/Loterie-carr.jpg" alt="Loterie Nationale">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(4)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/tilt-logo-fwb.jpg" alt="FEDERATION WALLONIE-BRUXELLES">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(5)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/tilt-logo-one.jpg" alt="OFFICE DE LA NAISSANCE ET DE L'ENFANCE">
+                        </div>
                     </div>
-                    <div class="viewPart" onclick="currentPart(2)">
-                        <img src="./assets/img/tilt-logo-spw.jpg" alt="Service public de Wallonie">
+                   
+                </div>
+                <div class="col-12">
+                    <div class="row">
+                        <div class="viewPart col" onclick="currentPart(6)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/kiroundg.gif" alt="KIWANIS INTERNATIONAL">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(7)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/tilt-logo-tvcom.jpg" alt="tV com BRABANT WALLON">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(8)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/Région-Wallonne.jpg" alt="REGION WALLONNE">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(9)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/tilt-logo-gazette-kid.jpg" alt="GAZETTE KIDS">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(10)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/tilt-logo-article27.jpg" alt="article 27">
+                        </div>
                     </div>
-                    <div class="viewPart" onclick="currentPart(3)">
-                        <img src="./assets/img/Loterie-carr.jpg" alt="Loterie Nationale">
-                    </div>
-                    <div class="viewPart" onclick="currentPart(4)">
-                        <img src="./assets/img/tilt-logo-fwb.jpg" alt="FEDERATION WALLONIE-BRUXELLES">
-                    </div>
-                    <div class="viewPart" onclick="currentPart(5)">
-                        <img src="./assets/img/tilt-logo-one.jpg" alt="OFFICE DE LA NAISSANCE ET DE L'ENFANCE">
-                    </div>
-                    <div class="viewPart" onclick="currentPart(6)">
-
-                    </div>
-                    <div class="viewPart" onclick="currentPart(7)">
-
-                    </div>
-                    <div class="viewPart" onclick="currentPart(8)">
-
-                    </div>
-                    <div class="viewPart" onclick="currentPart(9)">
-
-                    </div>
-                    <div class="viewPart" onclick="currentPart(10)">
-
-                    </div>
-                    <div class="viewPart" onclick="currentPart(11)">
-
-                    </div>
-                    <div class="viewPart" onclick="currentPart(12)">
-
-                    </div>
-                    <div class="viewPart" onclick="currentPart(13)">
-
-                    </div>
-                    <div class="viewPart" onclick="currentPart(14)">
-
-                    </div>
-                    <div class="viewPart" onclick="currentPart(15)">
-
+                </div>
+                <div class="col-12">
+                    <div class="row">
+                        <div class="viewPart col" onclick="currentPart(11)">
+                            <img class="logo cursor mx-auto d-block" src="./assets/img/tilt-logo-cap48.jpg" alt="CAP 48">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(12)">
+                        <img class="logo cursor mx-auto d-block" src="./assets/img/110114logocommunerixensart.jpg" alt="BRABANT WALLON COMMUNE DE RIXENSART">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(13)">
+                        <img class="logo cursor mx-auto d-block" src="./assets/img/2013-GSK-Logo-with-Strapline.jpg" alt="GSK do more feel better live longer">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(14)">
+                        <img class="logo cursor mx-auto d-block" src="./assets/img/logo_reine.paola_.jpg" alt="Fondation Reine Paola">
+                        </div>
+                        <div class="viewPart col" onclick="currentPart(15)">
+                        <img class="logo cursor mx-auto d-block" src="./assets/img/round_square_logo_fr_be.png" alt="Le Petit Moutard">
+                        </div>
                     </div>
                 </div>
             </div>
