16be1480e752b8fbb09a7b5e2b81ac2978bc59ac
wilfort <wilfort.stephane@hotmail.fr>
2018-08-10 17:38:20 +0200

    project php letilt

--- assets/vue/mail.php
+++ assets/vue/mail.php
@@ -34,7 +34,7 @@
         if($fichierTXT=="oui"){
             $mail->addAttachment('./assets/message.txt');  
         }
-        $mail->addAttachment('./assets/uploads/'.$photo, $photo);
+        $mail->addAttachment('/assets/uploads/'.$photo, $photo);
 
         $mail->isHTML(true);
         $mail->Body = $donnerMessage;
