ea65ac9d9d29f1593670fc3b35d610b8ba9d542a
wilfort <wilfort.stephane@hotmail.fr>
2018-08-10 19:33:43 +0200

    project php letilt

--- contact.php
+++ contact.php
@@ -2,9 +2,9 @@
      //uncomment for Steaphane's version of phpmailer
     // include ("assets/vue/traitement-mail.php");
      //uncomment for version of mailer +upload by Son
-    include ("./assets/php/upload.php");
-    include ("./assets/php/mailer.php");
     
+    include ("/assets/php/mailer.php");
+    include ("/assets/php/upload.php");
 
 ?>
 <!DOCTYPE html>
