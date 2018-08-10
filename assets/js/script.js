8bf1e48279ea25664a8b75354317e943519f0c00
wilfort <wilfort.stephane@hotmail.fr>
2018-08-11 00:50:40 +0200

    multipage website in php letilt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('./service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
-  }
+  // window.addEventListener('load', function () {
+     navigator.serviceWorker.register('service-worker.js').then(function (registration) {
+       // Registration was successful
+       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+     }, function (err) {
+       // registration failed :(
+       console.log('ServiceWorker registration failed: ', err);
+     });
+   }
