e9dfa1a46e6dd7a625fc059901e003d977021b82
wilfort <wilfort.stephane@hotmail.fr>
2018-08-11 01:31:02 +0200

    multipage website in php letilt

--- service-worker.js
+++ service-worker.js
@@ -200,7 +200,7 @@ self.addEventListener('install', function(e) {
       return cache.addAll(urlsToCache);
     })
   );
-});
+});/*
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
@@ -214,7 +214,7 @@ self.addEventListener('activate', function(e) {
     })
   );
   return self.clients.claim();
-}); 
+}); */
 /*self.addEventListener('fetch', function(e) {
   console.log('[ServiceWorker] Fetch', e.request.url);
   e.respondWith(
@@ -223,11 +223,11 @@ self.addEventListener('activate', function(e) {
     })
   );
 });*/
-
+/*
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
   var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
-  if (e.request.url.indexOf(dataUrl) > -1) {
+  if (e.request.url.indexOf(dataUrl) > -1) {*/
     /*
      * When the request URL contains dataUrl, the app is asking for fresh
      * weather data. In this case, the service worker always goes to the
@@ -235,7 +235,7 @@ self.addEventListener('fetch', function(e) {
      * network" strategy:
      * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
      */
-    e.respondWith(
+ /*   e.respondWith(
       caches.open(dataCacheName).then(function(cache) {
         return fetch(e.request).then(function(response){
           cache.put(e.request.url, response.clone());
@@ -243,19 +243,19 @@ self.addEventListener('fetch', function(e) {
         });
       })
     );
-  } else {
+  } else {*/
     /*
      * The app is asking for app shell files. In this scenario the app uses the
      * "Cache, falling back to the network" offline strategy:
      * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
      */
-    e.respondWith(
+ /*   e.respondWith(
       caches.match(e.request).then(function(response) {
         return response || fetch(e.request);
       })
     );
   }
-});
+});*/
 /*
 self.addEventListener('install', function (event) {
   // Perform install steps
