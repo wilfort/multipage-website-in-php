3a13aaafdc88d57441b142a204530366298f794a
wilfort <wilfort.stephane@hotmail.fr>
2018-08-11 23:25:05 +0200

    multipage website in php LETILT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
-// if ('serviceWorker' in navigator) {
-//    window.addEventListener('load', function () {
-//      navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
-//        // Registration was successful
-//        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//      }, function (err) {
-//        // registration failed :(
-//        console.log('ServiceWorker registration failed: ', err);
-//      });
-//    });
-//   }
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-             .register('multipage-website-letilt/service-worker.js')
-             .then(function() { console.log('Service Worker Registered'); });
+if ('serviceWorker' in navigator) {
+   window.addEventListener('load', function () {
+     navigator.serviceWorker.register('./service-worker.js').then(function (registration) {
+       // Registration was successful
+       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+     }, function (err) {
+       // registration failed :(
+       console.log('ServiceWorker registration failed: ', err);
+     });
+   });
   }
+  // if ('serviceWorker' in navigator) {
+  //   navigator.serviceWorker
+  //            .register('service-worker.js')
+  //            .then(function() { console.log('Service Worker Registered'); });
+  // }
