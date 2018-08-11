c6e059d0dfb6144d93f690f2f264b5308dabe4c4
wilfort <wilfort.stephane@hotmail.fr>
2018-08-11 23:53:13 +0200

    multipage website in php LETILT

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
-if ('serviceWorker' in navigator) {
-   window.addEventListener('load', function () {
-     navigator.serviceWorker.register('./assets/js/service-worker.js').then(function (registration) {
-       // Registration was successful
-       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-     }, function (err) {
-       // registration failed :(
-       console.log('ServiceWorker registration failed: ', err);
-     });
-   });
-  }
+// if ('serviceWorker' in navigator) {
+//    window.addEventListener('load', function () {
+//      navigator.serviceWorker.register('./assets/js/service-worker.js').then(function (registration) {
+//        // Registration was successful
+//        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//      }, function (err) {
+//        // registration failed :(
+//        console.log('ServiceWorker registration failed: ', err);
+//      });
+//    });
+//   }
   // if ('serviceWorker' in navigator) {
   //   navigator.serviceWorker
   //            .register('service-worker.js')
