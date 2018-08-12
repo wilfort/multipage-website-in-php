f7178901151d6e033a8616e7a0485973afd1e591
wilfort <wilfort.stephane@hotmail.fr>
2018-08-12 15:15:35 +0200

    multipage website in php LETILT

--- service-worker.js
+++ service-worker.js
@@ -216,7 +216,7 @@ self.addEventListener('activate', function(e) {
 }); 
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  var dataUrl = 'https://http://multipage-website-letilt.herokuapp.com/';
   if (e.request.url.indexOf(dataUrl) > -1) {
     e.respondWith(
       caches.open(dataCacheName).then(function(cache) {
